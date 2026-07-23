0f91ab3a90aae3eb0055f5e2d0bb5347cdb4e5c1
Illia Tkach <tkillia06@gmail.com>
2026-07-22 21:41:14 -0400

    add MapLibre map centered on the user's current location

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,122 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { use, useEffect, useState } from "react";
+import Map, { Marker } from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [viewState, setViewState] = useState({
+    longitude: 0,
+    latitude: 0,
+    zoom: 15,
+  });
+
+  const [userLongitude, setUserLongitude] = useState(0)
+  const [userLatitude, setUserLatitude] = useState(0)
+
+
+  function getUserLocation() {
+    // Check if the browser supports the Geolocation API
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }
+
+  // Handles successful location retrieval
+  function successCallback(position: any) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setViewState({ longitude: longitude, latitude: latitude, zoom: 15 });
+    setUserLatitude(latitude);
+    setUserLongitude(longitude);
+
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+  }
+
+  // Handles errors (e.g., user denied permission)
+  function errorCallback(error: any) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        console.error("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.error("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        console.error("The request to get user location timed out.");
+        break;
+      default:
+        console.error("An unknown error occurred.");
+        break;
+    }
+  }
+
+  useEffect(() => {
+    let cancelled = false;
+    const controller = new AbortController();
+
+    const fet = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch("/api/restaurants/pins", {
+          signal: controller.signal,
+        });
+
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        const json = await res.json();
+
+        if (!cancelled) setData(json);
+      } catch (err: any) {
+        if (err.name !== "AbortError" && !cancelled) setError(err);
+      } finally {
+        if (!cancelled) setLoading(false);
+      }
+    };
+
+    fet();
+    getUserLocation();
+
+    return () => {
+      cancelled = true;
+      controller.abort();
+    };
+  }, []);
+  console.log(data);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <div className="h-screen w-screen">
+      <Map
+        {...viewState}
+        onMove={(evt) => setViewState(evt.viewState)}
+        mapStyle="https://api.maptiler.com/maps/hybrid-v4/style.json?key=IVtsnHXRqn5BREdDdOlE"
+      >
+        <Marker longitude={userLongitude} latitude={userLatitude}>
+          <div className="h-4 w-4 rounded-full bg-blue-500  ring-2 ring-white shadow-[0_0_10px_#22d3ee,0_0_20px_#22d3ee] filter drop-shadow-[0_0_4px_rgba(34,211,238,0.5)]" />
+        </Marker>
+      </Map>
+    </div>
+  );
 }
 
-export default App
+export default App;
